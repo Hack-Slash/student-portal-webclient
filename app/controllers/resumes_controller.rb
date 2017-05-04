59db424a4c38ee70019cc32613fe5901b105e44a
Dov Bluming <dovybluming@gmail.com>
2017-05-03 23:43:29 -0400

    sessions 2

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,22 +1,6 @@
 class ResumesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @resumes = Unirest.get("http://localhost:3000/api/v1/students").body
-    p @resumes
-  end
-
-  def show
-  end
-
-  def editpassword
-    @student = User.find_by(student_id: params[:id])
-    render 'updatepassword.html.erb'
-  end
-
-  def updatepassword
-    @student = User.find_by(student_id: params[:id])
-    @student.update(
-      password: params[:password],
-      password_confirmation: params[:password_confirmation]
-    )
   end
 end
