d3fcea2ef77ea3e215e0dd85f38a7d288893956c
Dov Bluming <dovybluming@gmail.com>
2017-05-04 20:12:37 -0400

    ccc

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,7 @@
 class ResumesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @resumes = Unirest.get("http://localhost:3000/api/v1/students").body
+    @resume = Unirest.get("http://localhost:3000/api/v1/students/#{current_user.student_id}").body
   end
+ 
 end
