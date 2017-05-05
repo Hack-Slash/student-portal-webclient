437e8236c493287ad061931ef326ef862bb7d294
j-eaves <jdeaves@gmail.com>
2017-05-04 20:54:55 -0400

    modify edit.html.erb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,17 +19,17 @@ class StudentsController < ApplicationController
   def update
   	@student = Unirest.patch("http://localhost:3000/api/v1/students").body.first
   	@resume.update(
-	  first_name: params[:first_name] || @student['first_name'], 
+      first_name: params[:first_name] || @student['first_name'], 
       last_name: params[:last_name] || @student['last_name'], 
       email: params[:email] || @student['email'],
       phone_number: params[:phone_number] || @student['phone_number'],
       short_bio: params[:short_bio] || @student['short_bio'],
       linkedin_url: params[:linkedin_url] || @student['linkedin_url'],
       twitter_handle: params[:twitter_handle] || @student['twitter_handle'],
-	  personal_web_url: params[:personal_web_url] || @student['personal_web_url'],
-	  online_resume_url: params[:online_resume_url] || @student['online_resume_url'],
-	  github_url: params[:github_url] || @student['github_url'],
-	  photo: params[:photo] || @student['photo']
+      personal_web_url: params[:personal_web_url] || @student['personal_web_url'],
+      online_resume_url: params[:online_resume_url] || @student['online_resume_url'],
+      github_url: params[:github_url] || @student['github_url'],
+      photo: params[:photo] || @student['photo']
       )
   	redirect_to "/resumes"
   end
